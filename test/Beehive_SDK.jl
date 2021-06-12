f1847bd010420252e126ebe8f43a9c5816f4e349
dehann <dehann.fourie@gmail.com>
2021-06-12 12:07:16 -0400

    update to honeycomb

--- test/Beehive_SDK.jl
+++ test/Beehive_SDK.jl
@@ -9,12 +9,12 @@ using NavAbilitySDK
 
 
 # To get this, open app.navability.io, log in, press F12, go to Application, click Local Storage, click https://app.navability.io, click idToken, copy the value of ID token into here.
-token = "" 
+token = ""
 
 cdfg = CloudDFG(token=token)
 
 # build a graph with 20 poses
-RoME.generateCanonicalFG_Beehive!(20,graphinit=false, fg=cdfg)
+RoME.generateCanonicalFG_Honeycomb!(5,graphinit=false, fg=cdfg)
 
 
 
