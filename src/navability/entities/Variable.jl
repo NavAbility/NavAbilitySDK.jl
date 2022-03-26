a7e3a3e57a762299b3ecd1518472ef23a251fcf6
Sam Claassens <sclaassens.ad@gmail.com>
2022-03-26 16:26:55 -0500

    Parametrizing SolverData

--- src/navability/entities/Variable.jl
+++ src/navability/entities/Variable.jl
@@ -3,9 +3,11 @@ using JSON
 
 DFG_VERSION = "0.18.1";
 
+#TODO: Is this used? If not, we should remove.
 @enum VariableType begin
     POINT2
     POSE2
+    CONTINUOUSSCALAR
 end
 
 struct Variable
@@ -43,30 +45,47 @@ struct SolverDataDict
     solvedCount::Int
     solveKey::String
 end
+function SolverDataDict(variableType::String, solveKey::String, dims::Int)
+    return SolverDataDict(            
+        zeros(dims*100),
+        dims,
+        zeros(dims),
+        dims,   
+        [],
+        collect(range(0,dims-1, step=1)),
+        dims,
+        false,
+        "_null",
+        [],
+        variableType,
+        false,
+        zeros(dims),
+        false,
+        false,
+        0,
+        0,
+        solveKey)
+end
+
+"""
+Internal utility function to create the correct solver data (variable data)
+given a variable type.
+"""
+function _getSolverDataDict(variableType::String, solveKey::String)::SolverDataDict
+    if variableType == "RoME.Point2"
+        return SolverDataDict(variableType, solveKey, 2)
+    end
+    if variableType == "RoME.Pose2"
+        return SolverDataDict(variableType, solveKey, 3)
+    end
+    if variableType == "IncrementalInference.ContinuousScalar"
+        return SolverDataDict(variableType, solveKey, 1)
+    end
+    throw(error("Variable type '$(variableType)' not supported."))
+end
 
 function Variable(label::String, type::String, tags::Vector{String} = ["VARIABLE"], timestamp::String = string(now(Dates.UTC))*"Z")::Variable
-    solverDataDict = Dict(
-        "default" => SolverDataDict(
-            [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-            3,
-            [0.0,0.0,0.0],
-            3,   
-            [],
-            [0,1,2],
-            3,
-            false,
-            "_null",
-            [],
-            type,
-            false,
-            [0.0,0.0,0.0],
-            false,
-            false,
-            0,
-            0,
-            "default"
-        )
-    )
+    solverDataDict = Dict("default" => _getSolverDataDict(type, "default"))
     result = Variable(
         label,
         "{}",
