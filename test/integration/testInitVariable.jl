d1a4ec12e59b4da30ed66b09c9f638809ee634f4
dehann <dehann.fourie@gmail.com>
2022-08-12 12:35:18 -0700

    improve testing Pose3 and initVariable

--- test/integration/testInitVariable.jl
+++ test/integration/testInitVariable.jl
@@ -3,18 +3,22 @@
 using TensorCast
 
 
-function runInitVariableTests(;client = NVA.NavAbilityHttpsClient(;authorize=false))
+function runInitVariableTests(;
+    client = NVA.NavAbilityHttpsClient(;authorize=false),
+    userId = "guest@navability.io",
+    robotId = "TESTING",
+    sessionId = "INITVARIABLE_"*(string(NVA.uuid4())[1:4])
+  )
+  #
   @testset "run initVariable tests" begin
     
     ##
-    userId = "guest@navability.io"
-    robotId = "TESTING"
-    sessionId = "INITVARIABLE_"*(string(NVA.uuid4())[1:4])
     # connections
     @show context = NVA.Client(userId,robotId,sessionId)
     
-    resultId = NVA.addVariable(client, context, NVA.Variable("x0", :Pose2))
+    resultId = NVA.addVariable(client, context, NVA.Variable("x0", :Pose2)) |> fetch
     # Wait for them to be done before proceeding.
+    @info "Wait on addVariable eventId" resultId
     NVA.waitForCompletion(client, [resultId], expectedStatuses=["Complete"], maxSeconds=180)
     
     # init some value to variable x0
@@ -25,7 +29,6 @@ function runInitVariableTests(;client = NVA.NavAbilityHttpsClient(;authorize=fal
     variableWhere = NVA.VariableWhere(sessionKey, "x0", NVA.POSE2)
     particleInput = Dict{String,Any}("points"=>points)
     distributionInput = Dict{String,Any}("particle"=>particleInput)
-    # distr = Dict{String,Any}("distribution"=>distributionInput)
     initVarInp = NVA.InitVariableInput(variableWhere, distributionInput)
 
     eventId = NVA.initVariable(client, context, initVarInp) |> fetch
