b7a776e3321e5fe06369bd6b568d6478d3e1ef9f
dehann <dehann.fourie@gmail.com>
2022-08-25 23:13:57 -0700

    default nothing option

--- src/navability/entities/Solve.jl
+++ src/navability/entities/Solve.jl
@@ -5,7 +5,7 @@ Solver options including the solve key and whether the
 parametric solver should be used.
 """
 Base.@kwdef struct SolveOptions
-    key::Union{String, Nothing}
+    key::Union{String, Nothing} = nothing
     useParametric::Bool = false
 end
 
