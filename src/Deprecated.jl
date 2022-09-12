4add965a8a6a4c0c60bdf95894c0fb0abf81a84b
dehann <dehann.fourie@gmail.com>
2022-09-12 09:58:12 -0700

    typo

--- src/Deprecated.jl
+++ src/Deprecated.jl
@@ -3,5 +3,5 @@
 ## Remove below before v0.6
 ## =========================
 
-@deprecate getData(client::NavAbilityClient, context::Client, fileId::AbstactString) getData(client, context, UUID(fileId))
+@deprecate getData(client::NavAbilityClient, context::Client, fileId::AbstractString) getData(client, context, UUID(fileId))
 @deprecate getDataByLabel( client::NavAbilityClient, context::Client, vlbl::AbstractString, w...; kw...) getData(client, context, vlbl, w...; kw...)
