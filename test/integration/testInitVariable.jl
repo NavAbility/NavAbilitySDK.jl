fdd3c6b7501659ed6c1650042f1b8229b1f0f0ea
Jim hill <jim@navability.io>
2022-09-14 19:04:13 -0700

    InitVariable passing

--- test/integration/testInitVariable.jl
+++ test/integration/testInitVariable.jl
@@ -25,26 +25,24 @@ function runInitVariableTests(;
     pts = [[-rand();rand();-0.1] for _ in 1:5]
     points = Dict{String,Float64}[ NVA.CartesianPointInput(;x=pt[1],y=pt[2],rotz=pt[3]) for pt in pts ]
 
-    sessionKey = NVA.SessionKey(userId,robotId,sessionId)
-    variableWhere = NVA.VariableWhere(sessionKey, "x0", NVA.POSE2)
+    variableKey = NVA.VariableKey(userId,robotId,sessionId,"x0")
+    variableId = NVA.VariableId(variableKey)
     particleInput = Dict{String,Any}("points"=>points)
     distributionInput = Dict{String,Any}("particle"=>particleInput)
-    initVarInp = NVA.InitVariableInput(variableWhere, distributionInput)
+    initVarInp = NVA.InitVariableInput(variableId, NVA.POSE2, distributionInput)
 
     eventId = NVA.initVariable(client, context, initVarInp) |> fetch
     @info "waitForCompletion on initVariable" eventId 
-    NVA.waitForCompletion2(client, eventId) #, expectedStatuses=["Complete"], maxSeconds=180)
-
-    res = NVA.getVariable(client, context, "x0")
-    x0 = fetch(res)
-
-    # FIXME cannot remember which way round the reshape should work
-    x0_vv = reshape(x0["solverData"][1]["vecval"],3,:) 
-    @cast refvv[i][d] := x0_vv[d,i]
-
-    @show refvv
-
-    @test isapprox(pts, refvv; atol=1e-8)
+    completedSuccessfully = NVA.waitForCompletion2(client, eventId) #, expectedStatuses=["Complete"], maxSeconds=180)
+    @test completedSuccessfully
+    if completedSuccessfully
+      res = NVA.getVariable(client, context, "x0")
+      x0 = fetch(res)
+      x0_vv = reshape(x0["solverData"][1]["vecval"],3,:) 
+      @cast refvv[i][d] := x0_vv[d,i]
+      @show pts, refvv
+      @test isapprox(pts, refvv; atol=1e-12)
+    end
   end
 end
 
