f8a3eb974a49ab5a8fffe696cf0ce575a22fb2cf
Johannes Terblanche <Affie@users.noreply.github.com>
2023-05-04 10:12:27 +0200

    Start work on supporting Maps and Workflows

--- src/NavAbilitySDK.jl
+++ src/NavAbilitySDK.jl
@@ -88,6 +88,7 @@ include("graphql/UserRobotSession.jl")
 include("graphql/Factor.jl")
 include("graphql/Variable/Variable.jl")
 include("graphql/BlobStore.jl")
+include("graphql/Map.jl")
 
 include("entities/Distributions.jl")
 include("entities/InferenceTypes.jl")
@@ -95,6 +96,7 @@ include("entities/VariableTypes.jl")
 include("entities/UserRobotSession.jl")
 include("entities/Variable.jl")
 include("entities/Factor.jl")
+include("entities/Map.jl")
 
 include("NavAbilityClient.jl")
 
@@ -108,6 +110,7 @@ include("services/BlobEntry.jl")
 include("services/BlobStore.jl")
 include("services/StandardAPI.jl")
 include("services/FactorGraph.jl")
+include("services/Map.jl")
 include("services/Workers.jl")
 
 include("services/AsyncCalls.jl")
