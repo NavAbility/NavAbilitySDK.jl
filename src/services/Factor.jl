c053afde007605aed84df79c58347043883b1ef8
Johannes Terblanche <6612981+Affie@users.noreply.github.com>
2024-10-25 09:30:16 +0200

    Add addFactors and better listFactors (#266)
    Co-authored-by: Johannes Terblanche <Affie@users.noreply.github.com>

--- src/services/Factor.jl
+++ src/services/Factor.jl
@@ -1,145 +1,122 @@
 #TODO factor does not have blobs yet
 
-function addFactor!(
+function addFactor!(fgclient::NavAbilityDFG, pacfac::PackedFactor)
+    return addFactors!(fgclient, [pacfac])[1]
+end
+
+function DFG.addFactors!(
     fgclient::NavAbilityDFG,
-    pacfac::PackedFactor;
-    variableLabels::Vector{<:Union{Symbol, String}} = pacfac._variableOrderSymbols,
+    pacfactors::Vector{PackedFactor};
+    chunksize::Int = 20,
 )
-    client = fgclient.client.client
+    addfactors = map(pacfactors) do pacfac
+        FactorCreateInput(;
+            getCommonProperties(FactorCreateInput, pacfac)...,
+            id = getId(fgclient, pacfac),
+            variables = createConnect(
+                map(vl -> getId(fgclient, fgclient.fg, vl), pacfac._variableOrderSymbols),
+            ),
+            fg = createConnect(getId(fgclient.fg)),
+        )
+    end
 
-    factorLabel = pacfac.label
-
-    namespace = fgclient.fg.namespace
-    fgLabel = fgclient.fg.label
-    fgId = NvaSDK.getId(namespace, fgLabel)
-
-    variablesConnect = Dict(
-        "connect" => map(variableLabels) do vlink
-            Dict(
-                "where" => Dict(
-                    "node" => Dict(
-                        "id" => getId(namespace, fgLabel, vlink),
-                    ),
-                ),
-            )
-        end,
-    )
-
-    addfac = FactorCreateInput(;
-        getCommonProperties(FactorCreateInput, pacfac)...,
-        variables=variablesConnect,
-        id = getId(namespace, fgLabel, factorLabel),
-        fg = createConnect(fgId),
-    )
-
-    variables = Dict("factorsToCreate" => [addfac])
-    
     T = @NamedTuple{factors::Vector{PackedFactor}}
+    newfacs = @showprogress asyncmap(Iterators.partition(addfactors, chunksize)) do chunk
+        response =
+            executeGql(fgclient, GQL_ADD_FACTORS, Dict("factorsToCreate" => chunk), T)
+        handleMutate(response, "addFactors", :factors)
+    end
 
-    response = GQL.execute(
-        client,
-        GQL_ADD_FACTORS,
-        T;#FIXME: use factor response named tuple
-        # FactorResponse;
-        variables,
-        throw_on_execution_error = true,
-    )
-
-    return handleMutate(response, "addFactors", :factors)[1]
+    return reduce(vcat, newfacs)
 end
 
 function getFactors(fgclient::NavAbilityDFG)
-
     fgId = getId(fgclient.fg)
 
-    variables = Dict(
-        "fgId" => fgId,
-        "fields_summary" => true,
-        "fields_full" => true,
-    )
+    variables = Dict("fgId" => fgId, "fields_summary" => true, "fields_full" => true)
 
     T = Vector{Dict{String, Vector{PackedFactor}}}
 
-    response =
-        GQL.execute(fgclient.client.client, GQL_GET_FACTORS, T; variables, throw_on_execution_error = true)
+    response = executeGql(fgclient, GQL_GET_FACTORS, variables, T)
 
     return handleQuery(response, "factorgraphs", fgclient.fg.label)["factors"]
 end
-
 
 function getFactorsSkeleton(fgclient::NavAbilityDFG)
     fgId = getId(fgclient.fg)
 
-    variables = Dict(
-        "fgId" => fgId,
-        "fields_summary" => false,
-        "fields_full" => false,
-    )
+    variables = Dict("fgId" => fgId, "fields_summary" => false, "fields_full" => false)
 
     T = Vector{Dict{String, Vector{DFG.SkeletonDFGFactor}}}
 
-    response =
-        GQL.execute(fgclient.client.client, GQL_GET_FACTORS, T; variables, throw_on_execution_error = true)
+    response = executeGql(fgclient, GQL_GET_FACTORS, variables, T)
 
     return handleQuery(response, "factorgraphs", fgclient.fg.label)["factors"]
 end
 
-function getFactor(fgclient::NavAbilityDFG{<:AbstractDFGVariable, FT}, label::Symbol) where FT
-    
+function getFactor(
+    fgclient::NavAbilityDFG{<:AbstractDFGVariable, FT},
+    label::Symbol,
+) where {FT}
     namespace = fgclient.fg.namespace
     facId = NvaSDK.getId(namespace, fgclient.fg.label, label)
 
-    variables = Dict(
-        "facId" => facId,
-        "fields_summary" => true,
-        "fields_full" => true,
-    )
+    variables = Dict("facId" => facId, "fields_summary" => true, "fields_full" => true)
 
-    response = GQL.execute(
-        fgclient.client.client,
-        GQL_GET_FACTOR,
-        Vector{PackedFactor};
-        variables,
-        throw_on_execution_error = true,
-    )
-    #FIXME FT
-    # return FT(handleQuery(response, "factors", label))
-    return handleQuery(response, "factors", label)
+    response = executeGql(fgclient, GQL_GET_FACTOR, variables, Vector{PackedFactor};)
+    return FT(handleQuery(response, "factors", label))
 end
 
-function listFactors(fgclient::NavAbilityDFG)
+function listFactors(
+    fgclient::NavAbilityDFG,
+    regexFilter::Union{Nothing, Regex} = nothing;
+    tags::Vector{Symbol} = Symbol[],
+    solvable::Union{Int, Nothing} = nothing,
+    solvableFilter::Union{Nothing, Base.Fix2} = isnothing(solvable) ? nothing :
+                                                >=(solvable),
+)
+    fgId = NvaSDK.getId(fgclient.fg)
+    variables =
+        Dict("fgId" => fgId, "where" => Dict{String, Union{Int, Vector{Int}, Symbol}}())
 
-    fgId = getId(fgclient.fg)
+    if !isempty(tags)
+        @assert length(tags) == 1 "Only one tag is currently supported in tags filter"
+        variables["where"]["tags_INCLUDES"] = tags[1]
+    end
 
-    variables = Dict(
-        "fgId" => fgId
-    )
+    if !isnothing(solvableFilter)
+        if solvableFilter.f == >=
+            variables["where"]["solvable_GTE"] = solvableFilter.x
+        elseif solvableFilter.f == >
+            variables["where"]["solvable_GT"] = solvableFilter.x
+        elseif solvableFilter.f == <=
+            variables["where"]["solvable_LTE"] = solvableFilter.x
+        elseif solvableFilter.f == <
+            variables["where"]["solvable_LT"] = solvableFilter.x
+        elseif solvableFilter.f == ==
+            variables["where"]["solvable"] = solvableFilter.x
+        elseif solvableFilter.f == in
+            variables["where"]["solvable_IN"] = solvableFilter.x
+        else
+            error("Unsupported solvableFilter function: $(solvableFilter.f)")
+        end
+    end
 
-    T = Vector{Dict{String, Vector{@NamedTuple{label::Symbol}}}}
+    response = executeGql(fgclient, GQL_LIST_FACTORS, variables, Vector{Symbol})
+    labels = handleQuery(response, "listFactors")
 
-    response = GQL.execute(
-        fgclient.client.client,
-        GQL_LISTFACTORS,
-        T;
-        variables,
-        throw_on_execution_error = true,
-    )
+    !isnothing(regexFilter) && filter!(x -> occursin(regexFilter, string(x)), labels)
 
-    return last.(handleQuery(response, "factorgraphs", fgclient.fg.label)["factors"])
+    return labels
 end
 
 # delete factor and its satelites (by factor id)
 function deleteFactor!(fgclient::NavAbilityDFG, factor::DFG.AbstractDFGFactor)
     facId = getId(fgclient.fg, factor.label)
 
-    variables = (factorId=facId,)
+    variables = (factorId = facId,)
 
-    response = GQL.execute(
-        fgclient.client.client,
-        GQL_DELETE_FACTOR;
-        variables,
-        throw_on_execution_error = true,
-    )
+    response = executeGql(fgclient.client.client, GQL_DELETE_FACTOR, variables)
 
     #TODO check if factor was deleted in response
 
